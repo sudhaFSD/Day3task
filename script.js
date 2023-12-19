cdc7a3b42851e6ac7f7664b772bf8731da722f50
Sudha-FSD-Developer <154039291+sudhaFSD@users.noreply.github.com>
2023-12-19 19:41:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//How to compare two JSON have the same properties without order?
+// Ans: Convert both JSON objects to strings.
+
+//Parse the strings back to JSON objects.
+
+//Compare the resulting JSON objects.
